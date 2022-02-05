528fb8ec2d6b3001fe733da9b3faeafa0c5528d2
jogeshgupta963 <jogeshgupta963@gmail.com>
2022-02-06 01:15:26 +0530

    final commit

--- connection/connect.js
+++ connection/connect.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose")
 
 function dbConnect(url){
 
-    return mongoose.connect(url)
+    return mongoose.connect(url,{
+        useNewUrlParser: "true",
+        useUnifiedTopology: "true"
+    })
     
 }
 module.exports = dbConnect
